15d6ab6cb4e3c99f5e4aa952cbf68f0a8af520c7
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-13 21:24:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,9 @@ import store from './store';
 
 import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,document.getElementById('root'));
 
