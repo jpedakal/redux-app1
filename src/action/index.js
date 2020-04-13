4a39f9ece551b72b6c0c0c69e0d257c436c4cf2f
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-13 21:29:31 +0530

    update

---
+++ src/action/index.js
@@ -0,0 +1,9 @@
+export function movieList() {
+    return {
+        type: "Movie List",
+        payload: [{ id: 1, name: "chirutha" },
+        { id: 2, name: "magadheera" }]
+
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './container/App';
 import store from './store';
 
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
-  ,document.getElementById('root'));
+  , document.getElementById('root'));
 
